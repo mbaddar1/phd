71ed8118dc0601d785ffb1f9e00e845ed17ebf72
mbaddar <mohamed.baddar@here.com>
2022-01-08 16:29:56 +0100

    expressive power scripts

--- nf/expressive_power.py
+++
@@ -1,4 +0,0 @@
-"""
-http://proceedings.mlr.press/v108/kong20a/kong20a.pdf
-# TODO apply for CNF??
-"""
---
+++ nf/expressive_power/expressive_power_core.py
@@ -0,0 +1,28 @@
+"""
+References
+
+i) Express Power Normalizing Flows
+https://ucsdml.github.io/jekyll/update/2020/11/16/expressive-power-normalizing-flows.html
+http://proceedings.mlr.press/v108/kong20a/kong20a.pdf
+
+ii) Distance/ Similarity between probability distributions
+Divergence measures for statistical data processing
+https://hal.inria.fr/inria-00542337/document#:~:text=Distance%20or%20divergence%20measures%20are,diagnosis%2C%20model%20selection%20.%20.%20.
+
+lecture notes : Distances between probability measures
+https://souravchatterjee.su.domains//Lecture2.pdf
+
+lecture notes: Distances and Divergences for Probability Distributions
+https://nobel.web.unc.edu/wp-content/uploads/sites/13591/2020/11/Distance-Divergence.pdf
+# TODO
+"""
+
+
+class DistributionsDistance:
+    @staticmethod
+    def KLdiv(p, q):
+        pass
+
+    @staticmethod
+    def TotVarDist(p, q):
+        pass
